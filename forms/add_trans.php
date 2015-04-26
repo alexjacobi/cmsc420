99b099f524bb739f66cbb7d717614551c7adbfa1
Sahil Zubair <zubairs@vcu.edu>
2015-04-26 16:24:12 -0400

    editing transactions

--- forms/add_trans.php
+++ forms/add_trans.php
@@ -9,6 +9,24 @@ $_SESSION['password'] = $_POST['pass'];
 $query = mysql_query("SELECT * FROM accounts WHERE username = '$_SESSION[user]' and account_name = '$_SESSION[account_name]'");
 $row = mysql_fetch_array($query);
 
+
+if ($_POST[t] == 'edit') {
+	$query2 = mysql_query("SELECT amount FROM transactions WHERE id = '$_POST[id]'");
+	$row2 = mysql_fetch_array($query2);
+	if($_POST[type] == 'Income'){
+		$_amount = $_POST[amount];
+		$_current = $row[balance] + $_amount - $row2[0];
+}
+
+if($_POST[type] == 'Spending'){
+		$_amount = $_POST[amount] * -1;
+		$_current = $row[balance] + $_amount - $row2[0];
+}
+	mysql_query("DELETE FROM transactions where id='".$_POST[id]."'");
+	mysql_query("INSERT into transactions (username, account_name, transaction_type, amount, date, payee_comments, category, current_balance) values ('".$_SESSION['user']."',"."'".$_SESSION['account_name']."'".","."'".$_POST[type]."'".", '$_amount',"."'".$_POST[date]."'".","."'".$_POST[comments]."'".","."'".$_POST[category]."', $_current)");
+	mysql_query("UPDATE accounts SET balance = '$_current' WHERE username = '$_SESSION[user]' AND account_name = '$_SESSION[account_name]'");
+}
+else {
 if($_POST[type] == 'Income'){
 	$_amount = $_POST[amount];
 	$_current = $row[balance] + $_amount;
@@ -18,12 +36,6 @@ if($_POST[type] == 'Spending'){
 	$_amount = $_POST[amount] * -1;
 	$_current = $row[balance] + $_amount;
 }
-if ($_POST[t] == 'edit') {
-	mysql_query("DELETE FROM transactions where id='".$_POST[id]."'");
-	mysql_query("INSERT into transactions (username, account_name, transaction_type, amount, date, payee_comments, category, current_balance) values ('".$_SESSION['user']."',"."'".$_SESSION['account_name']."'".","."'".$_POST[type]."'".", '$_amount',"."'".$_POST[date]."'".","."'".$_POST[comments]."'".","."'".$_POST[category]."', $_current)");
-	mysql_query("UPDATE accounts SET balance = '$_current' WHERE username = '$_SESSION[user]' AND account_name = '$_SESSION[account_name]'");
-}
-else {
 mysql_query("INSERT into transactions (username, account_name, transaction_type, amount, date, payee_comments, category, current_balance) values ('".$_SESSION['user']."',"."'".$_SESSION['account_name']."'".","."'".$_POST[type]."'".", '$_amount',"."'".$_POST[date]."'".","."'".$_POST[comments]."'".","."'".$_POST[category]."', $_current)");
 mysql_query("UPDATE accounts SET balance = '$_current' WHERE username = '$_SESSION[user]' AND account_name = '$_SESSION[account_name]'");
 }
