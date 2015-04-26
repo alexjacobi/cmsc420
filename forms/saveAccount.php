3a8e25cc512669b07cb6c795047b01d7cff37f69
alexjacobi <alexjacobi1993@gmail.com>
2015-04-26 15:29:58 -0400

    small changes

--- forms/saveAccount.php
+++ forms/saveAccount.php
@@ -7,21 +7,25 @@ $_accountType = $_POST[type];
 $_remove = $_POST[remove];
 $_alter = $_POST[submitBtn];
 $_new = $_POST[name];
-
-$_accountName = $_SESSION['account_name'];
 $_user = $_SESSION['user'];
 
-$query = mysql_query("SELECT * FROM usertable where email = '$_user'") or die(mysql_error());
-$row = mysql_fetch_array($query);
+$query = mysql_query("SELECT * FROM accounts where username = '$_user' AND account_name = '$_accountName'") or die(mysql_error());
+
+while($row = mysql_fetch_array($query)){
+	if($_accountName == $row[account_name] && $_accountType ==  $row[account_type]) {
+		header('Refresh: 5; URL=../addNewAccount.php');
+		die("The account name and type you entered is already in use. You will now e redirected back to the previous screen.");
+	}
+}
 if($_alter == 'Save New Account') {
-	$query1 = mysql_query("INSERT INTO accounts (username, account_name, account_type, balance) VALUES ('".$row[email]."', '$_accountName', '$_accountType', 0)");
+	$query1 = mysql_query("INSERT INTO accounts (username, account_name, account_type, balance) VALUES ('$_user', '$_accountName', '$_accountType', 0)");
 }
 if($_alter == 'Remove Selected Account') {
 	$query2 = mysql_query("DELETE FROM accounts WHERE account_name = '$_remove'");
 }
 if($_alter == 'Submit') {
 	$query3 = mysql_query("UPDATE accounts SET account_name = '$_new' WHERE username = '$_user' AND account_name = '$_accountName'");
-	$query4 = mysql_query("UPDATE transactions SET account_name = '$_new' WHERE username = '$_user' AND account_name = '$_accountName '");
+	$query4 = mysql_query("UPDATE transactions SET account_name = '$_new' WHERE username = '$_user' AND account_name = '$_accountName'");
 }
 
 
