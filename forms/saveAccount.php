c6f6bafa2b493e62110f919f3065b0e7d48519a6
alexjacobi <alexjacobi1993@gmail.com>
2015-04-26 15:47:08 -0400

    edit acct name

--- forms/saveAccount.php
+++ forms/saveAccount.php
@@ -8,6 +8,7 @@ $_remove = $_POST[remove];
 $_alter = $_POST[submitBtn];
 $_new = $_POST[name];
 $_user = $_SESSION['user'];
+$_old = $_SESSION['account_name'];
 
 $query = mysql_query("SELECT * FROM accounts where username = '$_user' AND account_name = '$_accountName'") or die(mysql_error());
 
@@ -24,8 +25,8 @@ if($_alter == 'Remove Selected Account') {
 	$query2 = mysql_query("DELETE FROM accounts WHERE account_name = '$_remove'");
 }
 if($_alter == 'Submit') {
-	$query3 = mysql_query("UPDATE accounts SET account_name = '$_new' WHERE username = '$_user' AND account_name = '$_accountName'");
-	$query4 = mysql_query("UPDATE transactions SET account_name = '$_new' WHERE username = '$_user' AND account_name = '$_accountName'");
+	$query3 = mysql_query("UPDATE accounts SET account_name = '$_new' WHERE username = '$_user' AND account_name = '$_old'");
+	$query4 = mysql_query("UPDATE transactions SET account_name = '$_new' WHERE username = '$_user' AND account_name = '$_old'");
 }
 
 
